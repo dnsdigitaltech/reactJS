911d98f5afc6ef7884774130f68412e62b79f2ca
Davi Bernardo <dnssites@gmail.com>
2021-11-11 11:03:19 -0300

    Renderização condicional

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro';
 
 class App extends Component {
-    
+
+    constructor (props) {
+        super (props);
+        this.state = {
+            status: 1,
+            status2: true
+        };
+
+        this.sair = this.sair.bind(this);
+        this.entrar = this.entrar.bind(this);
+    }
+
+    sair() {
+        this.setState({status2:false})
+    }
+
+    entrar() {
+        this.setState({status2:true})
+    }
+
     render() {
         return (
-            <div>
-                <Membro nome="visitante"/>
+            <div>   
+                <div>
+                    <h2>Curso React JS</h2>
+                </div>  
+
+                {this.state.status === 1 && 
+                    <h1>Bem vindo ao sistema</h1>
+                }
+
+                {this.state.status2 ? 
+                    <div>
+                        <h1>Bem vindo ao sistema</h1>
+                        <button onClick={this.sair}>Sair</button>
+                    </div>:
+                    <div>
+                        <h1>Olá visitante</h1>
+                        <button onClick={this.entrar}>Entrar no sistema</button>
+                    </div>
+                }
             </div>
         );
     }
