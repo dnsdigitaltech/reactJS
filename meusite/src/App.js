061ce4e172528269a6a29620ddcab3e6ab5858d6
Davi Bernardo <dnssites@gmail.com>
2021-11-11 10:22:24 -0300

    Ciclos de vida dos Componentes

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,45 +1,32 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+   
     constructor (props) {
-        super (props);
-        this.state = {
-            nome: 'Davi',
-            contador: 0
+        super(props);
+        this.state = { 
+            hora: '00:00:00'
         };
-
-        this.aumentar = this.aumentar.bind(this);
-        this.diminuir = this.diminuir.bind(this);
     }
 
-    aumentar() {
-        let state = this.state;
-        state.contador += 1;
-        state.nome = 'Bernardo';
-        this.setState(state)
+    componentDidMount(){ //monta
+        setInterval (() => {
+            this.setState({ hora: new Date().toLocaleTimeString() })
+        }, 1000);
     }
 
-    diminuir () {
-        let state = this.state;
-        //não ficar numero negativos
-        if(state.contador ===0) {
-            alert('Opa chegou a zero!');
-            return;
-        }
-        state.contador -= 1;
-        state.nome = 'Davi';
-        this.setState(state)
+    componentDidUpdate(){ //atualiza
+        console.log('Atualizou!!')
+    }
+
+    shouldComponentDidUpdate(){ //retorna true or false
+
     }
 
     render() {
         return (
             <div>
-                <h1>Contador</h1>
-                    {this.state.nome}
-                <h3>
-                    <button onClick={this.diminuir}>-</button>
-                        {this.state.contador}
-                    <button onClick={this.aumentar}>+</button></h3>
+                <h1>Meu Projeto {this.state.hora}</h1>
             </div>
         );
     }
