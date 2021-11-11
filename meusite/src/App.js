ea5bcbd0c8f5b966e80adfcc8501e6e31202949a
Davi Bernardo <dnssites@gmail.com>
2021-11-11 09:37:57 -0300

    Aplicando Class component

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,47 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre nome={props.nome} 
-                    cargo={props.cargo} 
-                    idade={props.idade} />
-            <Social fb={props.facebook} 
-                    ln={props.linkedin}
-                    yt={props.youtube} />
-        </div>
-    )
+class Equipe extends Component {
+    render() {
+        return (
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
+                <Social />
+            </div>
+        );
+    }
 }
 
-const Sobre = (props) => {
-    return (
-        <div>
-            <h2>Olá sou o(a) {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade} anos</h3>
-        </div>
-    )
+class Sobre extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Olá sou o(a){this.props.nome}</h2>
+                <h3>Cargo: {this.props.cargo}</h3>
+                <h3>Idade: {this.props.idade} anos</h3>
+            </div>
+        );
+    }
 }
 
-const Social = (props) => {
+const Social = () => {
     return (
         <div>
-            <a href={props.fb}>Facebook</a>
-            <a href={props.ln}>LinkedIn</a>
-            <a href={props.yt}>Youtube</a>
-        </div>
-    )
+            <a>Facebook</a>
+            <a>LinkedIn</a>
+        </div>        
+    );
 }
-
 
 function App(){
     return (
         <div>
             <h1>Conheça nossa equipe</h1>
-            <Equipe nome="Davi" cargo="Programador" idade="37" 
-                    facebook="facebookDavi" linkedin="linkedinDavi" youtube="youtubeDavi"/>
-            <Equipe nome="Bernardo" cargo="Programador" idade="37" 
-                    facebook="facebookBernardo" linkedin="linkedinBernardo" youtube="youtubeBernardo"/>
+            <Equipe  nome="Davi" cargo="Programador" idade="37"/>
+            <Equipe  nome="Bernardo" cargo="Programador" idade="37"/>
         </div>
     )
 }
