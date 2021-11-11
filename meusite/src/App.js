aca8e24c2000cedb486692c86c5cd81c2d4e93eb
Davi Bernardo <dnssites@gmail.com>
2021-11-11 08:33:31 -0300

    Entendendo props e component

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const Bemvindo = (props) => {
+    return (
+        <div>
+            <h2>Bem vindo(a) {props.nome}</h2>
+            <h3>Tenho {props.idade} anos</h3>
+        </div>
+    )
+}
+
 function App(){
     return (
         <div>
             <h1>Olá mundo!</h1>
+            <Bemvindo nome ="Davi" idade="37"/>
+            <Bemvindo nome ="Bernardo" idade="12"/>
         </div>
     )
 }
