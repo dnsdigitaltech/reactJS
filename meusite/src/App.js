2d3b9ab7bbf6a51ce9bc12d4a75a0335717db128
Davi Bernardo <dnssites@gmail.com>
2021-11-11 10:44:01 -0300

    Eventos

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component {
-   
-    constructor (props) {
-        super(props);
-        this.state = { 
-            hora: '00:00:00'
-        };
-    }
-
-    componentDidMount(){ //monta
-        setInterval (() => {
-            this.setState({ hora: new Date().toLocaleTimeString() })
-        }, 1000);
-    }
-
-    componentDidUpdate(){ //atualiza
-        console.log('Atualizou!!')
-    }
-
-    shouldComponentDidUpdate(){ //retorna true or false
-
-    }
-
+    
     render() {
         return (
             <div>
-                <h1>Meu Projeto {this.state.hora}</h1>
+                <Membro nome="visitante"/>
             </div>
         );
     }
