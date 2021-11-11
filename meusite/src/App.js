89201439f55a94add653d88c1b17156d6c6d651d
Davi Bernardo <dnssites@gmail.com>
2021-11-11 08:59:28 -0300

    Entendendo props e component pt2

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,20 +1,47 @@
 import React from 'react';
 
-const Bemvindo = (props) => {
+const Equipe = (props) => {
     return (
         <div>
-            <h2>Bem vindo(a) {props.nome}</h2>
-            <h3>Tenho {props.idade} anos</h3>
+            <Sobre nome={props.nome} 
+                    cargo={props.cargo} 
+                    idade={props.idade} />
+            <Social fb={props.facebook} 
+                    ln={props.linkedin}
+                    yt={props.youtube} />
         </div>
     )
 }
 
+const Sobre = (props) => {
+    return (
+        <div>
+            <h2>Olá sou o(a) {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade} anos</h3>
+        </div>
+    )
+}
+
+const Social = (props) => {
+    return (
+        <div>
+            <a href={props.fb}>Facebook</a>
+            <a href={props.ln}>LinkedIn</a>
+            <a href={props.yt}>Youtube</a>
+        </div>
+    )
+}
+
+
 function App(){
     return (
         <div>
-            <h1>Olá mundo!</h1>
-            <Bemvindo nome ="Davi" idade="37"/>
-            <Bemvindo nome ="Bernardo" idade="12"/>
+            <h1>Conheça nossa equipe</h1>
+            <Equipe nome="Davi" cargo="Programador" idade="37" 
+                    facebook="facebookDavi" linkedin="linkedinDavi" youtube="youtubeDavi"/>
+            <Equipe nome="Bernardo" cargo="Programador" idade="37" 
+                    facebook="facebookBernardo" linkedin="linkedinBernardo" youtube="youtubeBernardo"/>
         </div>
     )
 }
